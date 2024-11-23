408b4e5431366f657ad8bcbadd39b952a1b26e5d
Sezimar Jr <sezimar_jrcs@hotmail.com>
2024-11-22 21:50:40 -0300

    fix: post serializer fixes

--- core/serializers/post_serializer.py
+++ core/serializers/post_serializer.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
 from core.models import Post
+from django.contrib.auth.models import User
 
 
 class PostSerializer(serializers.ModelSerializer):
+
+    user = serializers.StringRelatedField()
+
     class Meta:
         model = Post
         fields = '__all__'
+
+    def create(self, validated_data):
+        return Post.objects.create(**validated_data)
